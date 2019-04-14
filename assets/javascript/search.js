ed03686387444f0a0568c24dd570d9fbea3c90ac
LIZETHVERA <lizeth.veraro@gmail.com>
2019-04-14 13:41:33 -0700

    Apis file

--- assets/javascript/search.js
+++ assets/javascript/search.js
@@ -1,21 +1,23 @@
-$(document).on("click", ".btn-primary", function(event) {
+$(document).on("click", ".btn-primary", function (event) {
     event.preventDefault();
     displayRecipes();
-   
-   
+  
+
+
 
 });
+
 
 var chooseRecipe = true;
 var favorite;
 
 
 function displayRecipes() {
-
+ 
     var offset = Math.floor((Math.random() * 100) + 1);
     var recipe = $('.form-control-borderless').val();
     console.log(recipe);
-    
+
     var queryUrl = "https://api.edamam.com/search?q=" + recipe + "&app_id=45939a6b&app_key=e37d7f29462257f1fa878816ec76418f&limit=10&offset=" + offset;
 
     // Creating an AJAX call for the specific search button being clicked.
@@ -25,7 +27,7 @@ function displayRecipes() {
         method: "GET"
     }).then(function (response) {
 
-        
+
         // create a for to get the 10 elements from the data. 
         for (var i = 0; i < response.hits.length; i++) {
 
@@ -34,14 +36,27 @@ function displayRecipes() {
 
             showImage.data('title', response.hits[i].recipe.label);
 
+            var label = response.hits[i].recipe.label;
             var imgURL = response.hits[i].recipe.image;
-            var image = $("<img>").attr("src", imgURL);
-            showImage.append(image);
+            var image = $("<img>").attr("src", imgURL).attr('id',label);
 
-            $(".carousel-inner").append(showImage);
+            showImage.append(image);
             
+            $(".carousel-inner").append(showImage);
+           
         }
-       
+        
+
+        $(label).on("click", function () {
+            
+            var ingredientList = $('<ol>');
+            ingredientList.append($('<li>').text(response.hits[i].recipe.ingredientLines[i]));
+            console.log(ingredientList);
+
+            $(".list").append(ingredientList);
+        });
+
+
     });
 
 }
