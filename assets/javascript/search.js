9c28bfe6358966b6ea3c2b01462777f5b9d6c882
SITRUK <kurtis.santillanes@gmail.com>
2019-04-17 18:54:57 -0700

    saving local changes

--- assets/javascript/search.js
+++ assets/javascript/search.js
@@ -4,13 +4,7 @@ $(document).on("click", "#recipe-submit", function (event) {
     displayRecipes();
 });
 
-<<<<<<< HEAD
 var chooseRecipe = true;
-=======
-
-var
-    chooseRecipe = true;
->>>>>>> master
 var favorite;
 
 function displayRecipes() {
@@ -18,10 +12,6 @@ function displayRecipes() {
     var offset = Math.floor((Math.random() * 100) + 1);
     var recipe = $('#recipe-value').val();
     console.log(recipe);
-
-
-    var queryUrl = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/searchComplex?query=" + recipe + "&fillIngredients=true&instructionsRequired=true&addRecipeInformation=true&limitLicense=true&offset=" + offset + "&number=10&mashape-key=d20470ce32mshdbfd156afdd96dap16c402jsn7739274350bc";
-
 
     var queryUrl = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/searchComplex?query=" + recipe + "&fillIngredients=true&instructionsRequired=true&addRecipeInformation=true&limitLicense=true&" + offset + "=0&number=10&mashape-key=d20470ce32mshdbfd156afdd96dap16c402jsn7739274350bc";
     /*var queryUrl = "https://api.edamam.com/search?q=" + recipe + "&app_id=45939a6b&app_key=e37d7f29462257f1fa878816ec76418f&limit=10&offset=" + offset;*/
@@ -32,12 +22,6 @@ function displayRecipes() {
         method: "GET"
     }).then(function (response) {
 
-<<<<<<< HEAD
-        debugger;
-
-
-=======
->>>>>>> master
         // create a for to get the 10 elements from the data. 
         for (var i = 0; i < response.results.length; i++) {
 
@@ -46,7 +30,7 @@ function displayRecipes() {
             // Showing the image when the button is clicked.
             // ensuring 'active' class is added to first item in the loop
             // if (i === 0) {
-            // var showImage = $("<div class='carousel-item active' id = '"+id+"'>");
+            // var showImage = $("<div class='carousel-item active' id = '" + id + "'>");
             // }
             // Showing the image when the button in clicked.
             var showImage = $("<div class='carousel-item' id = '" + id + "'>");
@@ -57,21 +41,17 @@ function displayRecipes() {
             // creating a element image in DOM and attributte and specift ID. 
             var image = $("<img>").attr("src", imgURL).attr("id", id);
 
-                // var showTitle = $("<div class='carousel-caption d-md-block'>");
-                // var nameFood = $("<h5>").text(response.results[i].title);
-                // showTitle.append(nameFood);
-                // console.log(id);
+                var showTitle = $("<div class='carousel-caption d-md-block'>");
+                var nameFood = $("<h5>").text(response.results[i].title);
+                showTitle.append(nameFood);
+                console.log(id);
 
-<<<<<<< HEAD
             showImage.append(image);    
             $(showImage).append(showTitle); 
             // then add the like button / icon
             // $(".carousel-inner").append(favIcon);
             // then add the newly searched item in
-=======
-            showImage.append(image);
-            // $(showImage).append(showTitle); 
->>>>>>> master
+
             $(".carousel-inner").append(showImage);
 
             var descriptionOfFood = $("<h3 id = '" + id + "'>").text(response.results[i].title);
