252f4c297fc2c6662487b667086c716394ca27b0
LIZETHVERA <lizeth.veraro@gmail.com>
2019-04-17 21:59:33 -0700

    the recipes are working

--- assets/javascript/search.js
+++ assets/javascript/search.js
@@ -1,6 +1,6 @@
 $(document).on("click", ".btn-danger", function (event) {
     event.preventDefault();
-    debugger;
+    //debugger;
     displayRecipes();
 });
 
@@ -16,14 +16,10 @@ function displayRecipes() {
     var recipe = $('#recipe-value').val();
     console.log(recipe);
 
-<<<<<<< HEAD
-    var queryUrl = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/searchComplex?query=" + recipe + "&fillIngredients=true&instructionsRequired=true&addRecipeInformation=true&limitLicense=true&offset=" + offset + "&number=10&mashape-key=d20470ce32mshdbfd156afdd96dap16c402jsn7739274350bc";
-=======
->>>>>>> 16237b4a21bdecd5fb8c2d5b4be10f281f1ba35d
 
-    var queryUrl = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/searchComplex?query=" + recipe + "&fillIngredients=true&instructionsRequired=true&addRecipeInformation=true&limitLicense=true&" + offset + "=0&number=10&mashape-key=d20470ce32mshdbfd156afdd96dap16c402jsn7739274350bc";
+    var queryUrl = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/searchComplex?query=" + recipe + "&fillIngredients=true&instructionsRequired=true&addRecipeInformation=true&limitLicense=true&offset=" + offset + "&number=10&mashape-key=d20470ce32mshdbfd156afdd96dap16c402jsn7739274350bc";
     /*var queryUrl = "https://api.edamam.com/search?q=" + recipe + "&app_id=45939a6b&app_key=e37d7f29462257f1fa878816ec76418f&limit=10&offset=" + offset;*/
-    
+
     // Creating an AJAX call for the specific search button being clicked.
 
     $.ajax({
@@ -31,10 +27,7 @@ function displayRecipes() {
         method: "GET"
     }).then(function (response) {
 
-<<<<<<< HEAD
-=======
-        debugger;
->>>>>>> 16237b4a21bdecd5fb8c2d5b4be10f281f1ba35d
+        //debugger;
 
         // create a for to get the 10 elements from the data. 
         for (var i = 0; i < response.results.length; i++) {
@@ -42,48 +35,63 @@ function displayRecipes() {
             var id = response.results[i].id;
             // Showing the image when the button in clicked.
             var showImage = $("<div class='carousel-item' id = '" + id + "'>");
+            showImage.attr('data-name', response.results[i].title);
+            showImage.attr('data-index', i);
             // Id recipe from API
 
             // get the image from API
             var imgURL = response.results[i].image;
-            // creating a element image in DOM and attributte and specift ID. 
-            var image = $("<img>").attr("src", imgURL).attr("id", id);
 
-                // var showTitle = $("<div class='carousel-caption d-md-block'>");
-                // var nameFood = $("<h5>").text(response.results[i].title);
-                // showTitle.append(nameFood);
-                // console.log(id);
+            var image = $("<img>").attr("src", imgURL);
+
+            // var showTitle = $("<div class='carousel-caption d-md-block'>");
+            // var nameFood = $("<h5>").text(response.results[i].title);
+            // showTitle.append(nameFood);
+            // console.log(id);
 
             showImage.append(image);
-            // $(showImage).append(showTitle); 
-            $(".carousel-inner").append(showImage);
-
+            
             var descriptionOfFood = $("<h3 id = '" + id + "'>").text(response.results[i].title);
-            $("#" + id).append(descriptionOfFood);
+            showImage.append(descriptionOfFood)
 
-            
-            $("#" + id).on("click", function () {
-            
-                var idRecipe = $(this).attr('id');
-                console.log(idRecipe);
+            //$("#" + id).append(descriptionOfFood);
 
-                for (var i = 0; i < response.results.length; i++) {
+            //var ingredientsP = response.results[i].missedIngredients;
+            //console.log(ingredientsP);
+
+            showImage.on("click", function () {
                 
-                $("#currentName").append(response.results[i].title);
-                
-                var ingredientList = $("#current-recipe-list");
-                ingredientList.append($('<li>').text(response.results[i].missedIngredients[i].original));
-                console.log(ingredientList);
-                $(".list").append(ingredientList);
+                // var idRecipe = $(this).attr('id');
+                var recipeName = $(this).attr('data-name');
+                $("#currentName").append(recipeName);
 
+                var recipeIndex = $(this).attr('data-index');
+                /*
+                response = {"results":[ {..receta..}, {...receta..} ] }
+                respose.results = [ {..receta0..}, {...receta1..} ]
+                response.results[1] = {..receta1..}
+                response.results[1].missedIngredients = [ {..ingredient0..}. {..ingredient1..}, ..........]
+                response.results[1].missedIngredients.length = [].lenght = quantity of ingredient objects in the array
+                */
+               var recipe = response.results[recipeIndex];
+               console.log(recipe.missedIngredients.length);
+                for (var i = 0; i < recipe.missedIngredients.length; i++) {
+
+                    var ingredientList = $("#current-recipe-list");
+                    ingredientList.append($('<li>').text(recipe.missedIngredients[i].original));
+                    console.log(recipe.missedIngredients[i].original);
+                    $(".list").append(ingredientList);
                 }
+                
             });
+
+            $(".carousel-inner").append(showImage);
 
 
         }
 
- 
-        
+
+
 
 
     });
