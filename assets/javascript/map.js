b449ebed9ec9bdef353b00cdd46e88ceef0ae55b
Greg Lanter <dohnut22.gl@gmail.com>
2019-04-18 12:08:49 -0700

    changes for input validation on map search and cleaning up the look of the map

--- assets/javascript/map.js
+++ assets/javascript/map.js
@@ -4,7 +4,9 @@ var infowindow;
 
 //function initialize() {
 $("#map-btn").on("click", function () {
-    debugger;
+    $("#zip-input").removeClass("input-issue");
+    $("#state-input").removeClass("input-issue");
+    $("#city-input").removeClass("input-issue");
     $(".google-map-small").addClass("google-map-tall");
     $(".google-map-small").removeClass("google-map-small");
 
@@ -12,11 +14,22 @@ $("#map-btn").on("click", function () {
     var address = $("#address-input").val();
     var street = $("#street-input").val();
     var city = $("#city-input").val();
-    var state = $("#state-input").val();
-    var zip = $("#zip-input").val();
+    if (!char_input(city)) {
+        $("#city-input").addClass("input-issue");
+    }
 
-    address = address+"+"+street+"+"+city+"+"+state+"+"+zip;
-    var queryUrl = "https://maps.googleapis.com/maps/api/geocode/json?address="+address+"&key=AIzaSyBoFTlFdN-ElAc6fnn-M8hagD8cU0Lem38"
+    var state = $("#state-input").val();
+    if (!is_state(state)) {
+        $("#state-input").addClass("input-issue");
+    }
+
+    var zip = $("#zip-input").val();
+    if (!numeric_input(zip)||zip.length != 5) {
+        $("#zip-input").addClass("input-issue");
+    }
+
+    address = address + "+" + street + "+" + city + "+" + state + "+" + zip;
+    var queryUrl = "https://maps.googleapis.com/maps/api/geocode/json?address=" + address + "&key=AIzaSyBoFTlFdN-ElAc6fnn-M8hagD8cU0Lem38"
     //https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway+Mountain+View,+CA&key=YOUR_API_KEY
     var center;
 
@@ -25,9 +38,9 @@ $("#map-btn").on("click", function () {
         method: "GET"
     }).then(function (response) {
         console.log(response);
-        var lat =response.results[0].geometry.location.lat;
-        var lng =response.results[0].geometry.location.lng;
-        center = new google.maps.LatLng(lat, lng )
+        var lat = response.results[0].geometry.location.lat;
+        var lng = response.results[0].geometry.location.lng;
+        center = new google.maps.LatLng(lat, lng)
         map = new google.maps.Map(document.getElementById('googleMap'), {
             center: center,
             zoom: 12
@@ -76,5 +89,41 @@ $("#map-btn").on("click", function () {
         });
         return [latitude, longitude];
     }  */
+
+    function numeric_input(this_number) {
+        value = true;
+        var i = 0;
+
+        for (i = 0; i < this_number.length; i++) {
+            if (isNaN(parseInt(this_number[i]))) {
+                value = false;
+                break;
+            }
+        }
+        return value;
+    }
+
+    function is_state(this_state) {
+        var states = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "DC", "FL","GA","HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH","OK","OR","PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VI", "VA", "WA", "WV", "WI", "WY"]
+        if (states.indexOf(this_state.toUpperCase()) > -1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function char_input(this_char) {
+        value = true;
+        var i = 0;
+
+        for (i = 0; i < this_char.length; i++) {
+            if (parseInt(this_char[i])> -1) {
+                value = false;
+                break;
+            }
+        }
+        return value;
+    }
+
 });
 //google.maps.event.addDomListener(window, 'load', initialize);
